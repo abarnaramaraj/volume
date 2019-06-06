f0abb1f0b91dfa6d3917b0e551c20307f623024c
abarnaramaraj <47477481+abarnaramaraj@users.noreply.github.com>
2019-06-06 15:54:13 +0530

    Create Volume.java

---
+++ Volume.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+public class Volume
+{
+public static void main(String[] args)
+{
+Scanner sc=new Scanner(system.in);
+System.out.println("enter the length");
+double l=sc.nextDouble();
+System.out.println("enter the breath");
+double b=sc.nextDouble();
+system.out.println("enter the height");
+double h=sc.nextDouble();
+double area=(2*((l*b)+(b*h)+(h*l)));
+double volume=l*b*h;
+System.out.println(area);
+System.out.println(volume);
+}
+}
+
